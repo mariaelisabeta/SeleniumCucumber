ce53f21f72cd18f18dd7f723f905c7847ded7715
mariaelisabeta <gasparel.emaria@gmail.com>
2018-04-25 15:47:05 +0300

    added json report

--- src/test/java/org/fasttrackit/runners/ProductSortingRunner.java
+++ src/test/java/org/fasttrackit/runners/ProductSortingRunner.java
@@ -4,7 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @CucumberOptions(
-        plugin = {"html:target/cucumber"},
+        plugin = {"html:target/cucumber", "json:target/products-sorting.json"},
         glue={"org.fasttrackit"},
         features={"src/test/resources/features/products-sorting.feature"}
 )
